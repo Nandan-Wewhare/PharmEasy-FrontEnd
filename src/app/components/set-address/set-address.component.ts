26e9f728f3d5ed6ace40246e8c9e54f7e65995e3
nandan-wewhare <nandanwewhare25@gmail.com>
2022-10-17 10:42:10 +0530

    integrate update address

--- src/app/components/set-address/set-address.component.ts
+++ src/app/components/set-address/set-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth.service';
@@ -18,4 +19,31 @@ export class SetAddressComponent implements OnInit {
   ngOnInit(): void {}
 
   isLoading = false;
+
+  updateAddress(form: NgForm) {
+    this.isLoading = true;
+    this.authService
+      .updateAddress(form.value.address, form.value.pincode)
+      .subscribe({
+        next: (response: any) => {
+          this.isLoading = false;
+          if (response['status']) {
+            this.authService.setLoginData(
+              response['token'],
+              JSON.stringify(response['user'])
+            );
+            this.dialogRef.close();
+            this.snackBar.open('Address updated successfully!', '', {
+              duration: 2000,
+            });
+          } else {
+            this.snackBar.open(response['message'], '', { duration: 2000 });
+          }
+        },
+        error: (error) => {
+          this.isLoading = false;
+          this.snackBar.open(error['error']['message'], '', { duration: 2000 });
+        },
+      });
+  }
 }
