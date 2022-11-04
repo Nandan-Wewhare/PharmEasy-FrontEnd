8cfd5915f15a276d311e12e2e91898b1d495bd47
nandan-wewhare <nandanwewhare25@gmail.com>
2022-11-04 12:48:24 +0530

    integrate orders API & route

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -14,6 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 const materialComponents = [
   MatToolbarModule,
@@ -31,6 +32,7 @@ const materialComponents = [
   MatBadgeModule,
   MatProgressBarModule,
   MatMenuModule,
+  MatExpansionModule,
 ];
 
 @NgModule({
